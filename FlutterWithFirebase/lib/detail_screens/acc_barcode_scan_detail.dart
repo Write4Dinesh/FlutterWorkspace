8c32b271fa0812e0fb332e51b71557c5ba6aa774
Dinesh <dinesh@mymacbook.com>
2018-11-26 21:23:02 +0530

    implemented web search functionality

--- FlutterWithFirebase/lib/detail_screens/acc_barcode_scan_detail.dart
+++ FlutterWithFirebase/lib/detail_screens/acc_barcode_scan_detail.dart
@@ -4,6 +4,8 @@ import 'dart:io';
 import 'dart:async';
 import 'package:mlkit/mlkit.dart';
 import 'package:flutfire/utils/acc_app_constants.dart' as AppConstants;
+import 'package:flutfire/utils/widget_utility.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AccBarcodeScanDetail extends StatefulWidget {
   final File _file;
@@ -20,22 +22,29 @@ class _AccScanDetailState extends State<AccBarcodeScanDetail> {
   FirebaseVisionBarcodeDetector barcodeDetector =
       FirebaseVisionBarcodeDetector.instance;
   List<VisionBarcode> _currentBarcodeLabels = <VisionBarcode>[];
-
+  String _scannedBarcode;
   Stream sub;
-  StreamSubscription<dynamic> subscription;
+  StreamSubscription<dynamic>
+      subscription; // Subscribe for Stream of events that come into app asynchronously
 
+  /* called soon after the current widget is inserted into the widget tree*/
   @override
   void initState() {
     super.initState();
-    sub = new Stream.empty();
-    subscription = sub.listen((_) => _getImageSize)..onDone(analyzeLabels);
+    sub = new Stream
+        .empty(); // a Stream is a source of events.it emits events one after the other which will be handled by  StreamSubscription
+    subscription = sub.listen((_) => _getImageSize)
+      ..onDone(
+          analyzeLabels); //onDone is called when events finish. the handler method passed as argument
   }
 
   void analyzeLabels() async {
     try {
       var currentLabels;
-      currentLabels = await barcodeDetector.detectFromPath(widget._file.path);
+      String imageFilePath = widget._file.path;
+      currentLabels = await barcodeDetector.detectFromPath(imageFilePath);
       if (this.mounted) {
+        // mounted tells whether the current object is inserted into tree. it will be true until dispose is called(mounted=false)
         setState(() {
           _currentBarcodeLabels = currentLabels;
         });
@@ -45,6 +54,7 @@ class _AccScanDetailState extends State<AccBarcodeScanDetail> {
     }
   }
 
+/* removed this object/stateObject/widgetObject from the view tree..hence, mounted=false*/
   @override
   void dispose() {
     // TODO: implement dispose
@@ -59,47 +69,42 @@ class _AccScanDetailState extends State<AccBarcodeScanDetail> {
           centerTitle: true,
           title: Text(AppConstants.BARCODE_SCANNER_SCREEN_TITLE),
         ),
-        body: Column(
-          children: <Widget>[
-            buildBarcodeList<VisionBarcode>(_currentBarcodeLabels),
-          ],
+        body: Container(
+          child: buildBody<VisionBarcode>(_currentBarcodeLabels, context),
         ));
   }
 
-  Widget buildBarcodeList<T>(List<T> barcodes) {
+  Widget buildBody<T>(List<T> barcodes, BuildContext context) {
     if (barcodes.length == 0) {
-      return Expanded(
-        flex: 1,
-        child: Center(
-          child: Text('Nothing detected',
-              style: Theme.of(context).textTheme.subhead),
-        ),
+      return Center(
+        child: Text('Nothing detected',
+            style: Theme.of(context).textTheme.subhead),
       );
     }
-    return Expanded(
-      flex: 1,
-      child: Container(
-        child: ListView.builder(
-            padding: const EdgeInsets.all(1.0),
-            itemCount: barcodes.length,
-            itemBuilder: (context, i) {
-              var text;
-              final barcode = barcodes[i];
-              VisionBarcode res = barcode as VisionBarcode;
-              text = "Raw Value: ${res.rawValue}";
-              return _buildTextRow(text);
-            }),
+    final barcode = barcodes[0];
+    VisionBarcode res = barcode as VisionBarcode;
+    _scannedBarcode = res.rawValue;
+    return Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
+      Center(
+        child: new Text('The Scanned Barcode:$_scannedBarcode',
+            style: Theme.of(context).textTheme.subhead),
       ),
-    );
+      RaisedButton(
+          onPressed: () => onPressed(context, _scannedBarcode),
+          color: Colors.green,
+          textColor: Colors.white,
+          shape: WidgetUtility.getShape(5.0),
+          child: Text('Searc web'))
+    ]);
   }
 
-  Widget _buildTextRow(text) {
-    return ListTile(
-      title: Text(
-        "$text",
-      ),
-      dense: true,
-    );
+  void onPressed(BuildContext context, String searchQuery) async {
+    final String url = "http://www.google.com/#q=$searchQuery";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Couldn\'t launch the url $url';
+    }
   }
 
   Future<Size> _getImageSize(Image image) {
